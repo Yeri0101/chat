510618d8b8cc446279319afc58bbd94d661f0a48
yerinto martinez <yerinto.martinez@hotmail.fr>
2022-12-29 14:40:48 +0100

    add_chat_devise_controller

---
+++ app/controllers/chat_devise_controller.rb
@@ -0,0 +1,18 @@
+class ChatDeviseController < ApplicationController
+  class Responder < ActionController::Responder
+    def to_chat_stream
+      controller.render(options.merge(formats: :html))
+    rescue ActionView::MissingTemplate => e
+      if get?
+        raise e
+      elsif has_errors? && default_action
+        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+      else
+        redirect_to navigation_location
+      end
+    end
+  end
+
+  self.responder = Responder
+  respond_to :html, :chat_stream
+end
---
+++ app/helpers/chat_devise_helper.rb
@@ -0,0 +1,2 @@
+module ChatDeviseHelper
+end
